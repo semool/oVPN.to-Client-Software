7525850dd99cd210e2671066b11ef4d2e0538e08
MrNice <support@ovpn.to>
2016-07-09 18:52:26 +0200

    update release.bat

--- release.bat
+++ release.bat
@@ -15,10 +15,8 @@ copy /Y "%SOURCEDIR%\%EXESTRING%" "%BINSDIR%\%EXESTRING%"
 copy /Y "%SOURCEDIR%\ovpn_client.py" "%DEVDIST%\"
 copy /Y "%SOURCEDIR%\setup.py" "%DEVDIST%\"
 copy /Y "%SOURCEDIR%\7zSFXcfg%BITS%.txt" "%DEVDIST%\"
-copy /Y "%SOURCEDIR%\build.bat" "%DEVDIST%\"
-copy /Y "%SOURCEDIR%\debug.bat" %DEVDIST%\"
-copy /Y "%SOURCEDIR%\README.md" %DEVDIST%\"
-copy /Y "%SOURCEDIR%\release.bat" %DEVDIST%\"
+copy /Y "%SOURCEDIR%\*.bat" "%DEVDIST%\"
+copy /Y "%SOURCEDIR%\inno_setup%BITS%.iss" "%DEVDIST%\"
 
 set BUILDDEVFILE=%DEVSDIR%\build-dev-%VERSION%.7z
 IF EXIST %BUILDDEVFILE% (del %BUILDDEVFILE%)
