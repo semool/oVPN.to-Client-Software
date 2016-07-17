723009548e7a996be14e6f75b4cb19d147efa791
loomes <loomes@vulkanbox.sytes.net>
2016-07-17 14:17:16 +0200

    bugfix

--- includes_to_dist.bat
+++ includes_to_dist.bat
@@ -7,6 +7,7 @@ xcopy /Y /E "%INCLUDESDIR%\ico" "%DISTDIR%\ico\"
 xcopy /Y /E "%INCLUDESDIR%\dns" "%DISTDIR%\dns\"
 xcopy /Y /E "%LOCALEDIR%" "%DISTDIR%\locale\"
 xcopy /Y /E "%INCLUDESDIR%\themes" "%DISTDIR%\share\themes\"
+del "%DISTDIR%\etc\gtk-3.0\settings.ini"
 
 copy /Y "%INCLUDESDIR%\crypt32_win%BITS%.dll" "%DISTDIR%\crypt32.dll"
 
