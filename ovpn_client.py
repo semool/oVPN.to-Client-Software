2cac71054fa7af73ccd0e0404f4af82cafb19c35
loomes <loomes@vulkanbox.sytes.net>
2016-07-17 15:32:28 +0200

    fix it again

--- ovpn_client.py
+++ ovpn_client.py
@@ -2353,10 +2353,10 @@ class Systray:
 			self.nbpage2 = Gtk.VBox(False,spacing=2)
 			self.nbpage2.set_border_width(8)
 			self.nbpage2.pack_start(Gtk.Label(label=""),False,False,0)
-			self.settings_options_button_networkadapter(self.nbpage2)
 			self.settings_updates_button_normalconf(self.nbpage2)
 			self.settings_updates_button_forceconf(self.nbpage2)
 			self.settings_options_button_ipv6(self.nbpage2)
+			self.settings_options_button_networkadapter(self.nbpage2)
 			self.settings_updates_button_apireset(self.nbpage2)
 			self.settingsnotebook.append_page(self.nbpage2, Gtk.Label(_(" Updates ")))
 		except:
