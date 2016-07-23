dea90b4d72c85ebd664c89a2fd302eb291c6c764
MrNice <support@ovpn.to>
2016-07-23 18:33:44 +0200

    update bat files

--- includes_to_appdata.bat
+++ includes_to_appdata.bat
@@ -1,7 +1,10 @@
 @echo off
 IF NOT DEFINED INCLUDESDIR (echo "DO NOT RUN THIS FILE DIRECTLY" && PAUSE && EXIT)
 IF NOT DEFINED APPDIR (EXIT)
-xcopy /Y /E "%INCLUDESDIR%\dns" "%APPDIR%\dns\"
+
+:: dns needed for hardcoded dns updates only 
+:: xcopy /Y /E "%INCLUDESDIR%\dns" "%APPDIR%\dns\"
+
 xcopy /Y /E "%LOCALEDIR%" "%APPDIR%\locale\"
 copy /Y "%INCLUDESDIR%\cacert_ovpn.pem" "%APPDIR%\"
 
--- includes_to_dist.bat
+++ includes_to_dist.bat
@@ -3,15 +3,14 @@ IF NOT DEFINED INCLUDESDIR (echo "DO NOT RUN THIS FILE DIRECTLY" && PAUSE && EXI
 IF NOT DEFINED DISTDIR (EXIT)
 
 copy /Y "%INCLUDESDIR%\cacert_ovpn.pem" "%DISTDIR%\"
-xcopy /Y /E "%INCLUDESDIR%\dns" "%DISTDIR%\dns\"
 xcopy /Y /E "%LOCALEDIR%" "%DISTDIR%\locale\"
 xcopy /Y /E "%INCLUDESDIR%\themes" "%DISTDIR%\share\themes\"
 
-copy /Y "%INCLUDESDIR%\crypt32_win%BITS%.dll" "%DISTDIR%\crypt32.dll"
+::copy /Y "%INCLUDESDIR%\crypt32_win%BITS%.dll" "%DISTDIR%\crypt32.dll"
 
 ::Delete unneded Language Files 
 for /f "delims=" %%i in ('dir /b "%LANGPATH%*.*"') do (
-    IF NOT "%%i" == "de" IF NOT "%%i" == "en" IF NOT "%%i" == "es" IF NOT "%%i" == "nl" (
+    IF NOT "%%i" == "de" IF NOT "%%i" == "en" IF NOT "%%i" == "es" (
         rd /s /q "%LANGPATH%%%i" 2>nul
     )
 )
