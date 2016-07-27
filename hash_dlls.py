db074135d8e4474146f569755ebe7664e0ae84f0
MrNice <support@ovpn.to>
2016-07-27 20:25:03 +0200

    missed

--- hash_dlls.py
+++ hash_dlls.py
@@ -47,13 +47,16 @@ class HASH_DLLS:
 		self.DLL_HASH_DBS = { "UNSIGNED":self.DLL_DBF_HASH_UNSIGNED,"SIGNED":self.DLL_DBF_HASH_SIGNED }
 		try:
 			if not os.path.isdir(self.DLL_DIR):
-				os.mkdir(self.DLL_DIR)
-			
+				print "please create dir '%s'" % (self.DLL_DIR)
+				return False
+				
 			if not os.path.isdir(self.DLL_DIR_SIGNED):
-				os.mkdir(self.DLL_DIR_SIGNED)
-			
+				print "please create dir '%s'" % (self.DLL_DIR_SIGNED)
+				return False
+				
 			if not os.path.isdir(self.DLL_DIR_UNSIGNED):
-				os.mkdir(self.DLL_DIR_UNSIGNED)
+				print "please create dir '%s'" % (self.DLL_DIR_UNSIGNED)
+				return False
 		except:
 			print "could not create working dirs"
 			sys.exit()
@@ -110,43 +113,60 @@ class HASH_DLLS:
 	def hash_files(self):
 		for key,dir in self.DLL_DIRS.items():
 			if os.path.exists(dir):
-				print "def hash_files: dir = '%s'" % (dir)
 				files = {}
 				hash_files = self.list_files(dir)
-				for file in hash_files:
-					if os.path.isfile(file):
-						hash = self.hash_sha512_file(file)
-						if len(hash) == 128:
-							print "def hash_files: hashed file = '%s'" % (file)
-							file = file.split("\\")[-1]
-							files[file] = hash
-						else:
-							print "def hash_files: filepath '%s' failed" % (filepath)
-							sys.exit()
+				if len(hash_files) > 0:
+					print "def hash_files: dir = '%s'" % (dir)
+					for file in hash_files:
+						if os.path.isfile(file):
+							hash = self.hash_sha512_file(file)
+							if len(hash) == 128:
+								print "def hash_files: hashed file = '%s'" % (file)
+								file = file.split("\\")[-1]
+								files[file] = hash
+							else:
+								print "def hash_files: filepath '%s' failed" % (filepath)
+								sys.exit()
 				self.HASHS_DB[key] = files
 		return True
 
 	def verify_files(self):
 		for key,dir in self.DLL_DIRS.items():
 			if os.path.exists(dir):
-				print "\ndef verify_files: dir = '%s' vs. file '%s'" % (dir,self.DLL_HASH_DBS[key])
-				notfound, verified, failed  = 0, 0, 0
-				verify_files = self.list_files(dir)
-				for file in verify_files:
-					if os.path.isfile(file):
-						hash = self.hash_sha512_file(file)
-						file = file.split("\\")[-1]
-						if self.HASHS_DB[key][file] == hash:
-							verified += 1
-							print "def verify_files: file = '%s' OK" % (file)
+				notfound, verified, failed, missing  = 0, 0, 0, 0
+				verify_files_indir = self.list_files(dir)
+				len_verify_files_indir = len(verify_files_indir)
+				
+				try:
+					verify_files_store = self.HASHS_DB[key]
+				except:
+					verify_files_store = {}
+				
+				if len(verify_files_indir) > 0:
+					print "\ndef verify_files: dir = '%s' vs. file '%s'" % (dir,self.DLL_HASH_DBS[key])
+					
+					for file in verify_files_store:
+						fileshort = file.split("\\")[-1]
+						filepath = "%s\\%s" % (dir,file)
+						if not filepath in verify_files_indir:
+							notfound +=1
+							print "def verify_files: file = '%s\\%s' NOTFOUND" % (dir,file)
+					
+					for file in verify_files_indir:
+						fileshort = file.split("\\")[-1]
+						if fileshort in self.HASHS_DB[key]:
+							hash = self.hash_sha512_file(file)
+							file = file.split("\\")[-1]
+							if self.HASHS_DB[key][file] == hash:
+								verified += 1
+								#print "def verify_files: file = '%s' OK" % (file)
+							else:
+								failed += 1
+								print "def verify_files: file = '%s' FAILED" % (file)
 						else:
-							failed += 1
-							print "def verify_files: file = '%s' failed" % (file)
-					else:
-						notfound += 1
-						print "def verify_files: file = '%s' notfound" % (file)
-
-				print "def verify_files: key '%s' dir = '%s' notfound = '%s' failed = '%s' verified = '%s'" % (key, dir, notfound, failed, verified)
+							missing += 1
+							print "def verify_files: file = '%s' MISSING" % (file)
+				print "def verify_files: key '%s' dir = '%s' filesindir = '%s' filesjson = '%s' missing = '%s' notfound = '%s' failed = '%s' verified = '%s'" % (key, dir, len(verify_files_indir), len(verify_files_store), missing, notfound, failed, verified)
 
 def app():
 	HASH_DLLS()
