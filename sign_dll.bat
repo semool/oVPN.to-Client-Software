ffa899b17fa25f3bb7d183333d03f45d6d2c3117
semool <semool@protonmail.com>
2016-07-30 21:51:30 +0200

    include gtk3 theme dll (libunico.dll) to sign process

--- sign_dll.bat
+++ sign_dll.bat
@@ -24,6 +24,26 @@ for %%v in ("%DISTDIR%\*.dll") do (
 	)
 )
 
+for %%v in ("%DISTDIR%\lib\gtk-3.0\3.0.0\theming-engines\*.dll") do (
+	for %%A in ("%%v") do (
+		IF NOT EXIST %DLLDIR_S%\%%~nxA (
+			IF DEFINED SIGNTOOLCMD1 (%SIGNTOOLCMD1% "%%v")
+			IF DEFINED SIGNTOOLCMD4 (%SIGNTOOLCMD4% "%%v")
+			IF DEFINED SIGNTOOLVERI (
+				echo VERIFY: %%v
+				%SIGNTOOLVERI% "%%v"
+			)
+			IF NOT EXIST %DLLDIR_S% (
+				echo !!ATTENTION!! Create %DLLDIR_S% to save signed DLL
+			) else (
+				copy /Y "%%v" "%DLLDIR_S%\"
+			)
+		) else (
+			copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\lib\gtk-3.0\3.0.0\theming-engines\"
+		)
+	)
+)
+
 for %%v in ("%DISTDIR%\*.pyd") do (
 	for %%A in ("%%v") do (
 		IF NOT EXIST %DLLDIR_S%\%%~nxA (
