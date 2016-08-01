0dc0fe032dbf1348a1fe103946d1c19cdfde8871
MrNice <support@ovpn.to>
2016-08-01 02:26:51 +0200

    debug output

--- sign_dll.bat
+++ sign_dll.bat
@@ -7,6 +7,7 @@ IF NOT EXIST %SIGNTOOL% (echo %SIGNTOOL% NOT FOUND && PAUSE && EXIT)
 for %%v in ("%DISTDIR%\*.dll") do (
 	for %%A in ("%%v") do (
 		IF NOT EXIST %DLLDIR_U%\%%~nxA (
+			echo copy /Y "%%v" "%DLLDIR_U%\"
 			copy /Y "%%v" "%DLLDIR_U%\"
 		)
 		IF NOT EXIST %DLLDIR_S%\%%~nxA (
@@ -19,9 +20,11 @@ for %%v in ("%DISTDIR%\*.dll") do (
 			IF NOT EXIST %DLLDIR_S% (
 				echo !!ATTENTION!! Create %DLLDIR_S% to save signed DLL
 			) else (
+				echo copy /Y "%%v" "%DLLDIR_S%\"
 				copy /Y "%%v" "%DLLDIR_S%\"
 			)
 		) else (
+			echo copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\"
 			copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\"
 		)
 	)
@@ -30,6 +33,7 @@ for %%v in ("%DISTDIR%\*.dll") do (
 for %%v in ("%DISTDIR%\lib\gtk-3.0\3.0.0\theming-engines\*.dll") do (
 	for %%A in ("%%v") do (
 		IF NOT EXIST %DLLDIR_U%\%%~nxA (
+			echo copy /Y "%%v" "%DLLDIR_U%\"
 			copy /Y "%%v" "%DLLDIR_U%\"
 		)
 		IF NOT EXIST %DLLDIR_S%\%%~nxA (
@@ -42,9 +46,11 @@ for %%v in ("%DISTDIR%\lib\gtk-3.0\3.0.0\theming-engines\*.dll") do (
 			IF NOT EXIST %DLLDIR_S% (
 				echo !!ATTENTION!! Create %DLLDIR_S% to save signed DLL
 			) else (
+				echo copy /Y "%%v" "%DLLDIR_S%\"
 				copy /Y "%%v" "%DLLDIR_S%\"
 			)
 		) else (
+			echo copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\lib\gtk-3.0\3.0.0\theming-engines\"
 			copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\lib\gtk-3.0\3.0.0\theming-engines\"
 		)
 	)
@@ -65,9 +71,11 @@ for %%v in ("%DISTDIR%\*.pyd") do (
 			IF NOT EXIST %DLLDIR_S% (
 				echo !!ATTENTION!! Create %DLLDIR_S% to save signed DLL
 			) else (
+				echo copy /Y "%%v" "%DLLDIR_S%\"
 				copy /Y "%%v" "%DLLDIR_S%\"
 			)
 		) else (
+			echo copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\"
 			copy /Y "%DLLDIR_S%\%%~nxA" "%DISTDIR%\"
 		)
 	)
