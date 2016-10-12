3af9038fdf8da5d5991d7b0851b0ece5661d1355
MrNice <support@ovpn.to>
2016-10-12 15:43:21 +0200

    fixes

--- setup_check_bin.py
+++ setup_check_bin.py
@@ -84,15 +84,15 @@ setup_dict = dict(
 	options={
 		'py2exe': {
 		'dist_dir': DIST_DIR,
-		'bundle_files' : 3,
+		'bundle_files' : 1,
 		'optimize'     : 2,
 		'skip_archive' : False,
 		'compressed'   : False,
 		'unbuffered'   : False,
-		'includes'     : [ 'os','sys','time','hashlib','struct','subprocess','threading' ],
+		'includes'     : [ 'os','sys','time','hashlib','struct','subprocess','threading','zlib' ],
 		'excludes'     : release_version.setup_data()["py2exe_excludes"],
 		'packages'     : [ ],
-		'dll_excludes' : [ 'MSVCR100.dll','pywintypes27.dll','crypt32.dll','tcl85.dll', 'tk85.dll','DNSAPI.DLL','USP10.DLL','MPR.DLL','MSIMG32.DLL','API-MS-Win-Core-LocalRegistry-L1-1-0.dll','IPHLPAPI.DLL','w9xpopen.exe','mswsock.dll','powrprof.dll']
+		'dll_excludes' : [ 'pywintypes27.dll','crypt32.dll','tcl85.dll', 'tk85.dll','DNSAPI.DLL','USP10.DLL','MPR.DLL','MSIMG32.DLL','API-MS-Win-Core-LocalRegistry-L1-1-0.dll','IPHLPAPI.DLL','w9xpopen.exe','mswsock.dll','powrprof.dll']
 		}
 	}
 )
