0c912a4ba5f6857bde035a083b319f669d1923b8
semool <semool@protonmail.com>
2016-10-13 20:00:33 +0200

    Update README.md, patch_gtkdll.py, schedule_task.py for python3

--- schedule_task.py
+++ schedule_task.py
@@ -8,57 +8,63 @@ def set_task(DEBUG,AUTOSTART_DELAY_TIME):
 	DATE = datetime.datetime.now()
 	USERNAME = os.getenv('username')
 
+	lines = list()
+	lines.append('<?xml version="1.0" encoding="UTF-16"?>')
+	lines.append('<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">')
+	lines.append('  <RegistrationInfo>')
+	lines.append('    <Date>%s</Date>' % DATE.isoformat())
+	lines.append('    <Author>%s %s</Author>' % (release_version.org_data()["ORG"],release_version.org_data()["ADD"]))
+	lines.append('    <URI>\%s</URI>' % (release_version.version_data()["NAME"]))
+	lines.append('  </RegistrationInfo>')
+	lines.append('  <Triggers>')
+	lines.append('    <LogonTrigger>')
+	lines.append('      <Enabled>true</Enabled>')
+	lines.append('      <UserId>%s</UserId>' % USERNAME)
+	lines.append('      <Delay>PT%sS</Delay>' % AUTOSTART_DELAY_TIME)
+	lines.append('    </LogonTrigger>')
+	lines.append('  </Triggers>')
+	lines.append('  <Principals>')
+	lines.append('    <Principal id="Author">')
+	lines.append('      <UserId>%s</UserId>' % USERNAME)
+	lines.append('      <LogonType>InteractiveToken</LogonType>')
+	lines.append('      <RunLevel>HighestAvailable</RunLevel>')
+	lines.append('    </Principal>')
+	lines.append('  </Principals>')
+	lines.append('  <Settings>')
+	lines.append('    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>')
+	lines.append('    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>')
+	lines.append('    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>')
+	lines.append('    <AllowHardTerminate>false</AllowHardTerminate>')
+	lines.append('    <StartWhenAvailable>false</StartWhenAvailable>')
+	lines.append('    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>')
+	lines.append('    <IdleSettings>')
+	lines.append('      <StopOnIdleEnd>true</StopOnIdleEnd>')
+	lines.append('      <RestartOnIdle>false</RestartOnIdle>')
+	lines.append('    </IdleSettings>')
+	lines.append('    <AllowStartOnDemand>true</AllowStartOnDemand>')
+	lines.append('    <Enabled>true</Enabled>')
+	lines.append('    <Hidden>false</Hidden>')
+	lines.append('    <RunOnlyIfIdle>false</RunOnlyIfIdle>')
+	lines.append('    <WakeToRun>false</WakeToRun>')
+	lines.append('    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>')
+	lines.append('    <Priority>7</Priority>')
+	lines.append('  </Settings>')
+	lines.append('  <Actions Context="Author">')
+	lines.append('    <Exec>')
+	lines.append('      <Command>"%s\%s"</Command>' % (BIN_DIR,release_version.setup_data()["exename"]))
+	lines.append('      <WorkingDirectory>%s</WorkingDirectory>' % (BIN_DIR))
+	lines.append('    </Exec>')
+	lines.append('  </Actions>')
+	lines.append('</Task>')
+
 	XMLFILE = "%s\\autostart.xml" % (BIN_DIR)
 	remove_xml(DEBUG,XMLFILE)
-	ind = open(XMLFILE, "w")
-	print >> ind, '<?xml version="1.0" encoding="UTF-16"?>'
-	print >> ind, '<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">'
-	print >> ind, '  <RegistrationInfo>'
-	print >> ind, '    <Date>%s</Date>' % DATE.isoformat()
-	print >> ind, '    <Author>%s %s</Author>' % (release_version.org_data()["ORG"],release_version.org_data()["ADD"])
-	print >> ind, '    <URI>\%s</URI>' % (release_version.version_data()["NAME"])
-	print >> ind, '  </RegistrationInfo>'
-	print >> ind, '  <Triggers>'
-	print >> ind, '    <LogonTrigger>'
-	print >> ind, '      <Enabled>true</Enabled>'
-	print >> ind, '      <UserId>%s</UserId>' % USERNAME
-	print >> ind, '      <Delay>PT%sS</Delay>' % AUTOSTART_DELAY_TIME
-	print >> ind, '    </LogonTrigger>'
-	print >> ind, '  </Triggers>'
-	print >> ind, '  <Principals>'
-	print >> ind, '    <Principal id="Author">'
-	print >> ind, '      <UserId>%s</UserId>' % USERNAME
-	print >> ind, '      <LogonType>InteractiveToken</LogonType>'
-	print >> ind, '      <RunLevel>HighestAvailable</RunLevel>'
-	print >> ind, '    </Principal>'
-	print >> ind, '  </Principals>'
-	print >> ind, '  <Settings>'
-	print >> ind, '    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>'
-	print >> ind, '    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>'
-	print >> ind, '    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>'
-	print >> ind, '    <AllowHardTerminate>false</AllowHardTerminate>'
-	print >> ind, '    <StartWhenAvailable>false</StartWhenAvailable>'
-	print >> ind, '    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>'
-	print >> ind, '    <IdleSettings>'
-	print >> ind, '      <StopOnIdleEnd>true</StopOnIdleEnd>'
-	print >> ind, '      <RestartOnIdle>false</RestartOnIdle>'
-	print >> ind, '    </IdleSettings>'
-	print >> ind, '    <AllowStartOnDemand>true</AllowStartOnDemand>'
-	print >> ind, '    <Enabled>true</Enabled>'
-	print >> ind, '    <Hidden>false</Hidden>'
-	print >> ind, '    <RunOnlyIfIdle>false</RunOnlyIfIdle>'
-	print >> ind, '    <WakeToRun>false</WakeToRun>'
-	print >> ind, '    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>'
-	print >> ind, '    <Priority>7</Priority>'
-	print >> ind, '  </Settings>'
-	print >> ind, '  <Actions Context="Author">'
-	print >> ind, '    <Exec>'
-	print >> ind, '      <Command>"%s\%s"</Command>' % (BIN_DIR,release_version.setup_data()["exename"])
-	print >> ind, '      <WorkingDirectory>%s</WorkingDirectory>' % (BIN_DIR)
-	print >> ind, '    </Exec>'
-	print >> ind, '  </Actions>'
-	print >> ind, '</Task>'
+	ind = open(XMLFILE, "wt")
+	for line in lines:
+		ind.write("%s\n"%(line))
 	ind.close()
+	if not os.path.exists(XMLFILE):
+		return False
 
 	if os.path.exists(XMLFILE):
 		string = 'schtasks.exe /Create /XML "%s" /TN "%s"' % (XMLFILE,release_version.version_data()["NAME"])
