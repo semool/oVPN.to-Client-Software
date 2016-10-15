2a922882a788e07bfc123dacd1333c2076bd17be
MrNice <support@ovpn.to>
2016-10-15 22:59:44 +0200

    hires icon

--- win_notification.py
+++ win_notification.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from debug import debug
-import platform
-import time
+import os, platform, time
 
 # ATTENTION
 # win32api include in py2exe 0.9.2.2 give a import error pywintypes with python3.4.4. To fix it edit the Hooks file from py2exe:
@@ -10,7 +9,7 @@ import time
 # Now all works fine
 
 from win32api import GetSystemMetrics, GetModuleHandle, PostQuitMessage, LoadResource
-from win32con import SM_CXSMICON, SM_CYSMICON, CW_USEDEFAULT, IMAGE_ICON, IMAGE_BITMAP, IDI_INFORMATION, IDI_APPLICATION, LR_DEFAULTSIZE, LR_LOADFROMFILE, WM_DESTROY, WS_OVERLAPPED, WS_SYSMENU, WM_USER, RT_ICON
+from win32con import SM_CXICONSPACING, SM_CXSMICON, SM_CYSMICON, SM_CXICON, SM_CYICON, CW_USEDEFAULT, IMAGE_ICON, IMAGE_BITMAP, IDI_INFORMATION, IDI_APPLICATION, LR_DEFAULTSIZE, LR_LOADFROMFILE, WM_DESTROY, WS_OVERLAPPED, WS_SYSMENU, WM_USER, RT_ICON
 from win32gui import CreateIconFromResource, CreateWindow, DestroyWindow, LoadIcon, LoadImage, NIF_ICON, NIF_INFO, NIF_MESSAGE, NIF_TIP, NIM_ADD, NIM_DELETE, NIM_MODIFY, RegisterClass, UnregisterClass, Shell_NotifyIcon, UpdateWindow, WNDCLASS
 
 WINVER10 = False
@@ -50,19 +49,26 @@ class notify:
 			except Exception as e:
 				debug(222,"[win_notification.py] def send_notify: CreateIconFromResource() #1 failed, exception = '%s'"%(e),DEBUG,True)
 				try:
-					raise Exception
-					icon_path = "%s\\else\\app_icons\\shield_exe.ico" % (DEV_DIR)
-					#icon_path = "%s\\else\\app_icons\\app_layer.ico" % (DEV_DIR)
-					#icon_path = "%s\\else\\app_icons\\shield_exe_48.ico" % (DEV_DIR)
-					#icon_path = "%s\\else\\app_icons\\shield_exe_64.ico" % (DEV_DIR)
-					#icon_path = "%s\\else\\some_icons\\shield_256.bmp" % (DEV_DIR)
-					#icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE
+					icon_path = False
+					icon_path1 = "%s\\share\\icons\\shield_exe_32px_32bpp.ico" % (os.getcwd())
+					icon_path2 = "%s\\else\\app_icons\\shield_exe_32px_32bpp.ico" % (DEV_DIR)
+					files = [ icon_path1, icon_path2 ]
+					for file in files:
+							if os.path.isfile(file):
+								icon_path = file
+								break
+					if icon_path == False:
+						raise Exception
 					icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE
-					ico_x = GetSystemMetrics(SM_CXSMICON)
-					ico_y = GetSystemMetrics(SM_CYSMICON)
-					debug(1,"[win_notification.py] def send_notify: ico_x = '%s' ico_y = '%s'"%(ico_x,ico_y),DEBUG,True)
+					""" https://msdn.microsoft.com/en-us/library/windows/desktop/ms724385(v=vs.85).aspx """
+					#x1 = GetSystemMetrics(SM_CXSMICON)
+					#y1 = GetSystemMetrics(SM_CYSMICON)
+					#x2 = GetSystemMetrics(SM_CXICON)
+					#y2 = GetSystemMetrics(SM_CYICON)
+					#ispace = GetSystemMetrics(SM_CXICONSPACING)
+					#debug(222,"[win_notification.py] def send_notify: x1 = '%s' y1 = '%s', x2 = '%s' y2 = '%s', ispace = '%s'"%(x1,y1,x2,y2,ispace),DEBUG,True)
 					""" https://msdn.microsoft.com/en-us/library/windows/desktop/ms648045(v=vs.85).aspx """
-					hicon = LoadImage(self.hinst, icon_path,IMAGE_ICON, ico_x, ico_y, icon_flags)
+					hicon = LoadImage(self.hinst, icon_path,IMAGE_ICON, 0, 0, icon_flags)
 					debug(222,"[win_notification.py] def send_notify: LoadImage() #2",DEBUG,True)
 				except Exception as e:
 					debug(222,"[win_notification.py] def send_notify: LoadImage() #2 failed, exception = '%s'"%(e),DEBUG,True)
@@ -77,11 +83,15 @@ class notify:
 			flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
 			nid = (self.hwnd, 0, flags, WM_USER + 20, hicon, "Tooltip")
 			
-			anyreturn = Shell_NotifyIcon(NIM_ADD, nid)
-			debug(222,"[win_notification.py] def send_notify: Shell_NotifyIcon() #1 returned = '%s'"%(anyreturn),DEBUG,True)
+			Shell_NotifyIcon(NIM_ADD, nid)
 			
-			anyreturn = Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO,WM_USER + 20,hicon, "Balloon Tooltip", text, 200,title))
-			debug(222,"[win_notification.py] def send_notify: Shell_NotifyIcon() #2 returned = '%s'"%(anyreturn),DEBUG,True)
+			icontype = 0 # bad blurry icon
+			icontype = 1 # blue info
+			icontype = 2 # yellow exclamation
+			icontype = 3 # red cross
+			icontype = 4 # icon from filepath
+			
+			Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO,WM_USER + 20,hicon, "Balloon Tooltip", text, 200,title, icontype))
 			
 			if WINVER10 == False:
 				time.sleep(10)
