0bbf858b11206ee99c9fb31abd1b34a80c0ce597
MrNice <support@ovpn.to>
2016-11-01 03:20:46 +0100

    prepare 0.8.1

--- release_version.py
+++ release_version.py
@@ -5,7 +5,7 @@ BITS = struct.calcsize("P") * 8
 
 def version_data():
 	data = {
-			"VERSION" : "0.8.0",
+			"VERSION" : "0.8.1",
 			"SIGN" : True,
 			"NAME" : "%s Client" % (org_data()["ORG"]),
 		}
